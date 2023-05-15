5538118d3a6bdeadfb2d9de22724323fa527e798
Yermek Kapushev <kapushev@gmail.com>
2023-05-15 14:32:28 +0300

    Run eval several times with different random seeds

--- run_eval_proofs.py
+++ run_eval_proofs.py
@@ -1,18 +1,24 @@
 import os
+import random
 from pathlib import Path
 from pprint import pprint
 
+import numpy as np
 import torch
 import tqdm
 from jsonargparse import ArgumentParser
+from pytorch_lightning import seed_everything
 
 from lean_dataset import RMTDocsDataLoader
-from modeling_rmt.lightning import RMTModelPL
 from run_finetuning_lean_pl import (
-    get_cls_by_name,
+    get_base_model,
     get_datasets,
+    get_rmt_model,
     get_tokenizer,
 )
+
+max_seed_value = np.iinfo(np.uint32).max
+min_seed_value = np.iinfo(np.uint32).min
 
 
 def get_best_ckpt_path(ckpt_dir, n_segments):
@@ -25,9 +31,12 @@ def get_best_ckpt_path(ckpt_dir, n_segments):
 
     if not best_ckpt:
         if n_segments == 1:
-            best_ckpt = Path(ckpt_dir) / 'last.ckpt'
+            best_ckpt = Path(ckpt_dir) / "last.ckpt"
         else:
-            best_ckpt = Path(ckpt_dir) / f'last-v{n_segments - 1}.ckpt'
+            best_ckpt = Path(ckpt_dir) / f"last-v{n_segments - 1}.ckpt"
+
+    if not best_ckpt.exists():
+        return None
 
     return best_ckpt
 
@@ -42,77 +51,47 @@ def load_cfg(args, ckpt_path):
     return cfg
 
 
-def get_rmt_model(cfg, tokenizer):
-    rmt_config = {
-        "num_mem_tokens": cfg.num_mem_tokens,
-        "max_n_segments": cfg.max_n_segments,
-        "input_size": cfg.input_size,
-        "bptt_depth": cfg.bptt_depth,
-        "sum_loss": cfg.sum_loss,
-        "tokenizer": tokenizer,
-        "memory_forward_func": cfg.memory_forward_func,
-        "memory_layers": cfg.memory_layers,
-        "share_memory_layers": cfg.share_memory_layers,
-        "reconstruction_loss_coef": cfg.reconstruction_loss_coef,
-    }
-
-    # Load backbone model
-    backbone_cls = get_cls_by_name(cfg.backbone_cls)
-    backbone = backbone_cls.from_pretrained(cfg.backbone_cpt)
-
-    # Load RMT model
-    rmt_cls = get_cls_by_name(cfg.rmt_cls)
-    rmt_model = rmt_cls(backbone, **rmt_config)
-
-    pl_model = RMTModelPL.load_from_checkpoint(cfg.pretrained_ckpt, rmt_model=rmt_model)
-
-    try:
-        pl_model = torch.compile(pl_model)
-    except:
-        pass
-
-    return pl_model
-
-
-def eval_model(cfg, model, tokenizer, datasets, max_n_segments, batch_size, num_workers, proofs_only=True):
+def eval_model(
+    cfg, model, tokenizer, datasets, max_n_segments, batch_size, num_workers
+):
+    assert batch_size == 1
     losses = []
+    proofstep_indices = []
+    pad_start_indices = []
 
     device = next(iter(model.parameters())).device
 
-    with torch.inference_mode(), torch.autocast('cuda', dtype=torch.bfloat16):
+    with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
         model.eval()
-        model._module.reset_memory()
         datasets["val"].set_max_n_segments(max_n_segments)
         loader = RMTDocsDataLoader(
-            datasets["val"], batch_size=batch_size, drop_last=True,
-            num_workers=num_workers
+            datasets["val"],
+            batch_size=batch_size,
+            drop_last=True,
+            num_workers=num_workers,
         )
 
-        model._module._actual_max_n_segments = max_n_segments
-        model._module.reset_memory()
+        if hasattr(model._module, "reset_memory"):
+            model._module._actual_max_n_segments = max_n_segments
+            model._module.reset_memory()
         loss_sum = 0
         n_elements = 0
         with tqdm.tqdm(loader) as pbar:
             for batch in pbar:
                 batch = {k: v.to(device) for k, v in batch.items()}
-                labels = batch.pop('labels')
+                labels = batch.pop("labels")
                 out = model(batch)
 
                 proofstep_idx = torch.where(
                     batch["input_ids"] == tokenizer.vocab["[PROOFSTEP]"]
                 )
 
-                if proofs_only and not len(proofstep_idx[0]):
-                    continue
-
-                lm_logits = out.logits[:, cfg.num_mem_tokens : -cfg.num_mem_tokens]
+                lm_logits = out.logits
+                if not hasattr(cfg, "model_type") or cfg.model_type == "rmt":
+                    lm_logits = out.logits[:, cfg.num_mem_tokens : -cfg.num_mem_tokens]
 
                 shift_logits = lm_logits[..., :-1, :].contiguous()
                 shift_labels = labels[..., 1:].contiguous()
-
-                if proofs_only:
-                    for i, j in zip(*proofstep_idx):
-                        shift_labels[i.item()][: j.item() - 1] = -100
 
                 loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
                 loss = loss_fct(
@@ -121,29 +100,33 @@ def eval_model(cfg, model, tokenizer, datasets, max_n_segments, batch_size, num_
 
                 losses.append(loss.cpu())
 
+                if len(proofstep_idx[0]) > 0:
+                    proofstep_idx = proofstep_idx[1].item()
+                else:
+                    proofstep_idx = len(loss)
+
+                proofstep_indices.append(proofstep_idx)
+                pad_start_indices.append(batch["attention_mask"].sum().item())
+
                 loss_sum += loss.sum()
                 n_elements += len(loss)
                 pbar.set_postfix(loss=loss_sum / n_elements)
                 pbar.update(1)
 
-
-
         losses = torch.stack(losses, dim=0)
 
-    return losses
+    return losses, proofstep_indices, pad_start_indices
 
 
 def main():
-
     parser = ArgumentParser()
-    parser.add_argument('--run_id', type=str)
-    parser.add_argument('--n_segments_train', type=int, nargs="+")
-    parser.add_argument('--n_segments_val', type=int, nargs="+")
-    parser.add_argument('--proofs_only', type=bool, default=True)
-    parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--num_workers', type=int, default=1)
-    parser.add_argument('--working_dir', type=str, default='.')
-
+    parser.add_argument("--run_id", type=str, nargs="+")
+    parser.add_argument("--n_segments_train", type=int, nargs="+")
+    parser.add_argument("--n_segments_val", type=int, nargs="+")
+    parser.add_argument("--n_runs", type=int, default=1)
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--num_workers", type=int, default=1)
+    parser.add_argument("--working_dir", type=str, default=".")
 
     args = parser.parse_args()
 
@@ -158,59 +141,74 @@ def main():
 
     pprint(args.as_dict())
 
-    ckpt_dir = str(Path(working_dir) / f"logs/rmt_proofs/{args.run_id}/checkpoints/")
-    print(f"Run id: {args.run_id}")
-
     n_segments_train_list = args.n_segments_train
     n_segments_val_list = args.n_segments_val
+    run_id_list = args.run_id
 
-    save_filename_prefix = "proof_" if args.proofs_only else ""
-    print(save_filename_prefix)
-    for n_segments_train in n_segments_train_list:
+    for run_id in run_id_list:
+        ckpt_dir = str(Path(working_dir) / f"logs/rmt_proofs/{run_id}/checkpoints/")
+        print(f"Run id: {run_id}")
 
-        print(f"\tn_segments_train: {n_segments_train}")
-        print(ckpt_dir)
-        best_ckpt = get_best_ckpt_path(ckpt_dir, n_segments=n_segments_train)
+        save_filename_prefix = ""
+        for n_segments_train in n_segments_train_list:
+            print(f"\tn_segments_train: {n_segments_train}")
+            print(ckpt_dir)
+            best_ckpt = get_best_ckpt_path(ckpt_dir, n_segments=n_segments_train)
 
-        if best_ckpt is None:
-            return
+            if best_ckpt is None:
+                continue
 
-        cfg = load_cfg(args, best_ckpt)
-        cfg.pretrained_ckpt = best_ckpt
+            cfg = load_cfg(args, best_ckpt)
+            cfg.pretrained_ckpt = best_ckpt
 
-        pprint(cfg.as_dict())
+            if tokenizer is None:
+                tokenizer = get_tokenizer(cfg)
+                datasets = get_datasets(cfg, tokenizer)
 
-        if tokenizer is None:
-            tokenizer = get_tokenizer(cfg)
-            datasets = get_datasets(cfg, tokenizer)
+            if not hasattr(cfg, "model_type") or cfg.model_type == "rmt":
+                model = get_rmt_model(cfg, tokenizer)
+            elif cfg.model_type == "base":
+                model = get_base_model(cfg, tokenizer)
 
-        model = get_rmt_model(cfg, tokenizer)
-        model.to(device)
+            model.to(device)
 
-
-        for n_segments_val in n_segments_val_list:
-            total_loss = eval_model(
-                cfg, model, tokenizer, datasets, max_n_segments=n_segments_val, batch_size=args.batch_size,
-                num_workers=args.num_workers, proofs_only=args.proofs_only
-            )
-
-            mean_loss = total_loss.float().mean(dim=1)
-            perplexity = torch.exp(total_loss).mean()
-            print(
-                f"\ttrain_segments: {n_segments_train}, val_segments: {n_segments_val}, loss: {mean_loss.mean():.3f}, perplexity: {perplexity:.3f}"
-            )
-
-            torch.save(
-                total_loss,
-                str(
-                    Path(working_dir) / (
-                        f"logs/{save_filename_prefix}losses_"
-                        f"{args.run_id}_"
-                        f"{n_segments_train}_"
-                        f"{n_segments_val}.ckpt"
+            for n_segments_val in n_segments_val_list:
+                for run in range(args.n_runs):
+                    seed = random.randint(min_seed_value, max_seed_value)  # noqa: S311
+                    seed = seed_everything(seed)
+                    print(f"Using random seed: {seed}")
+                    total_loss, proofstep_idx, pad_start_idx = eval_model(
+                        cfg,
+                        model,
+                        tokenizer,
+                        datasets,
+                        max_n_segments=n_segments_val,
+                        batch_size=args.batch_size,
+                        num_workers=args.num_workers,
                     )
-                )
-            )
+
+                    mean_loss = total_loss.float().mean(dim=1)
+                    perplexity = torch.exp(total_loss).mean()
+                    print(
+                        f"\ttrain_segments: {n_segments_train}, val_segments: {n_segments_val}, loss: {mean_loss.mean():.3f}, perplexity: {perplexity:.3f}"
+                    )
+
+                    torch.save(
+                        {
+                            "loss": total_loss,
+                            "proofstep_idx": proofstep_idx,
+                            "pad_start_idx": pad_start_idx,
+                        },
+                        str(
+                            Path(working_dir)
+                            / (
+                                f"logs/{save_filename_prefix}losses_"
+                                f"{run_id}_"
+                                f"{n_segments_train}_"
+                                f"{n_segments_val}_{seed}.ckpt"
+                            )
+                        ),
+                    )
 
 
 if __name__ == "__main__":
